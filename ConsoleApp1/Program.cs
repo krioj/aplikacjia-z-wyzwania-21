bd9a5a9db7054003cb25141d346aa02981aae857
krioj <artemijkstnk@gmail.com>
2023-07-25 12:15:21 +0200

    Dzień 15: Abstrakcja i Interface

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -35,7 +35,7 @@ do
     Console.WriteLine($"Maksymalna ocena: {stats.Max}");
     Console.WriteLine($"Minimalna ocena: {stats.Min}");
     Console.WriteLine($"Suma ocen: {stats.Sum}");
-    char.TryParse(Console.ReadLine(), out q);
+    q = char.Parse(Console.ReadLine());
 } while ('q' != q);
 
 
